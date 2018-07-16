44ced99d0a2b65cd0b11d3a3d2aa6b85406e2eb0
Trevor Bennett <garbage@trevorbennett.us>
2018-07-16 19:16:24 -0400

    restructured the file system of the builder demo

---
+++ src/entities/House.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class House{
+    public int cost;
+    public String address;
+    public String owner;
+
+    public static class Builder {
+
+        private House house;
+        private int cost;
+        private String address;
+        private String owner;
+
+        public Builder hasCost(int cost) {
+            this.cost = cost;
+            return this;
+        }
+
+        public Builder hasAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+
+        public Builder hasOwner(String owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public House build() {
+            house = new House();
+            house.cost = this.cost;
+            house.address = this.address;
+            house.owner = this.owner;
+
+            return house;
+        }
+    }
+}
--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,50 +1,15 @@
 package main;
 
+import entities.House;
+
 public class HelloWorld{
 
     public static void main(String []args){
-        Builder.hasCost(1).hasAddress("Washington").hasOwner("Barry O").build();
-        System.out.println("Hello World");
-
-    }
-}
-
-public class House{
-    public int cost;
-    public String address;
-    public String owner;
-}
-
-public static class Builder{
-
-    private House house;
-    private int cost;
-    private String address;
-    private String owner;
-
-    public Builder hasCost(int cost){
-        this.cost = cost;
-        return this;
-    }
-
-    public Builder hasAddress(String address){
-        this.address = address;
-        return this;
-    }
-
-
-    public Builder hasOwner(String owner){
-        this.owner = owner;
-        return this;
-    }
-
-    public House build(){
-        house = new House();
-        house.cost = this.cost;
-        house.address = this.address;
-        house.owner = this.owner;
-
-        return house;
+        House house = new House.Builder().hasCost(1).hasAddress("Washington").hasOwner("Barry O").build();
+        System.out.println(house);
+        System.out.println(house.address);
+        System.out.println(house.cost);
+        System.out.println(house.owner);
     }
 }
 
